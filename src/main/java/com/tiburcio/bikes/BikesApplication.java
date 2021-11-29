0b6491f3f202f1c782441f65976f436d8f57b9f6
tcrurav <tiburcio.cruz@gmail.com>
2021-11-29 20:05:55 +0000

    first commit

---
+++ src/main/java/com/tiburcio/bikes/BikesApplication.java
@@ -0,0 +1,14 @@
+package com.tiburcio.bikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BikesApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiburcio/bikes/BikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiburcio.bikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
